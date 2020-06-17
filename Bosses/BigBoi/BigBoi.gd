3b62c0f0e6d6234722890a0f90142fff917edac9
KevinDNF <KevinNavarroFaubla@gmail.com>
2020-06-17 20:24:09 +0100

    added big boi animations

---
+++ Bosses/BigBoi/BigBoi.gd
@@ -0,0 +1,9 @@
+extends RigidBody2D
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_HitArea_body_entered(body: Node) -> void:
+	print("You hit big boi, you get big slap!")
+	pass # Replace with function body.
