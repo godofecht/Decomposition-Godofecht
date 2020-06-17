75dc6a6683262d157f20dcd85413edd16d746f12
godofecht <abhishek.shivakumar@gmail.com>
2020-06-17 22:23:44 +0100

    one more change

--- Bacteria/Bacteria.gd
+++ Bacteria/Bacteria.gd
@@ -94,7 +94,7 @@ func _process(delta):
 		bIncrementRecoveryTimer = false
 	if(bCanSeePlayer && recoverytimer == 0):
 		if(shootIntervalTimer >= shootIntervalTime):
-			shoot()
+#			shoot()
 			shootIntervalTimer = 0
 		followPlayer(player,delta)
 		
