f2e42731323c06d35eb1d8227329334cfd01dbe9
Abhishek Shivakumar <abhishek.shivakumar@gmail.com>
2024-12-10 11:45:30 +0000

    wew

--- DificultySelectionScreen/DificultyScreen.gd
+++ DificultySelectionScreen/DificultyScreen.gd
@@ -1,42 +1,39 @@
 extends Node2D
 
-var firstLevel = "res://World/Levels/Level1/Level1.tscn"
-#var firstLevel = "res://ProceduralMapGenerator/ProceduralMap.tscn"
-
-
-func _on_Easy_body_shape_entered(body_id: int, body: Node, body_shape: int, area_shape: int) -> void:
-	if(body.name == "Player"):
-		Global.player_health = 100
-		Global.TOTAL_HP = Global.player_health
-		Global.player_bullets = 10
-		startTransition()
-
-
-func _on_Hard_body_shape_entered(body_id: int, body: Node, body_shape: int, area_shape: int) -> void:
-	if(body.name == "Player"):
-		Global.player_health = 25
-		Global.TOTAL_HP = Global.player_health
-		Global.player_bullets = 3
-		startTransition()
-
-func _on_Insane_body_shape_entered(body_id: int, body: Node, body_shape: int, area_shape: int) -> void:
-	if(body.name == "Player"):
-		Global.player_health = 5
-		Global.TOTAL_HP = Global.player_health
-		Global.player_bullets = 1
-		startTransition()
-
-func startTransition():
-	$Transition.colorRect = $ColorRect
-	$Transition.fadeIn()
+#var firstLevel = "res://World/Levels/Level1/Level1.tscn"
+var firstLevel = "res://ProceduralMapGenerator/ProceduralMap.tscn"
 
 func changeLevel():
 	Global.player_crystals = 0
 	print("Moving to ")
 	print(firstLevel)
 	Global.CURRENT_LEVEL_SCENE = firstLevel
-	get_tree().change_scene(firstLevel)
+	get_tree().change_scene (firstLevel)
 
-
-func _on_Transition_tween_completed(object: Object, key: NodePath) -> void:
+func _on_Transition_tween_completed (object: Object, key: NodePath) -> void:
 	changeLevel()
+
+func _on_Easy_body_entered (body):
+	if(body.name == "Player"):
+		Global.player_health = 100
+		Global.TOTAL_HP = Global.player_health
+		Global.player_bullets = 10
+		startTransition()
+
+func _on_Hard_body_entered (body):
+	if(body.name == "Player"):
+		Global.player_health = 25
+		Global.TOTAL_HP = Global.player_health
+		Global.player_bullets = 3
+		startTransition()
+
+func _on_Insane_body_entered (body):
+	if(body.name == "Player"):
+		Global.player_health = 5
+		Global.TOTAL_HP = Global.player_health
+		Global.player_bullets = 1
+		startTransition()
+		
+func startTransition():
+	$Transition.colorRect = $ColorRect
+	$Transition.fadeIn()
--- ProceduralMapGenerator/ProceduralMap.gd
+++ ProceduralMapGenerator/ProceduralMap.gd
@@ -10,8 +10,11 @@ var _tileset
 
 #Randomize these later
 var topLeftCorner = Vector2(1,5)
-var width
-var height
+var width = 0
+var height = 0
+
+var prevWidth = 0
+var prevHeight = 0
 
 var num_rooms
 
@@ -24,33 +27,28 @@ var random = RandomNumberGenerator.new()
 var xPos
 var tile_pos
 
+var previousTopLeft = Vector2(0,0)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Initialize stuff here
 	_tileset = get_tileset()
-	
-	
 	num_rooms = random.randi_range(6,20)
-	
-
-
-	
-	
 	#GenerateEachRoomHere.
-	var n = 4
+	var n = 8
 	while n>0:
 		random.randomize()
 		var r_nextRoomDirection = 0
-		r_nextRoomDirection = random.randi_range(0,1)
-		createRoom(topLeftCorner,r_nextRoomDirection)
+		r_nextRoomDirection = random.randi_range (0,1)
+		createRoom (topLeftCorner,r_nextRoomDirection)
 		
 
 		if(r_nextRoomDirection == 0):
 			topLeftCorner.x += width
-			topLeftCorner.y += 4
+		#	topLeftCorner.y += 4
 			n -= 1
 		else:
-			topLeftCorner.x += 6
+		#	topLeftCorner.x += 6
 			topLeftCorner.y += height
 			n -= 1
 	
@@ -62,56 +60,61 @@ func _process(delta):
 	pass
 	
 
-func createRoom(topLeftCorner,r_nextRoomDirection):
+func createRoom (topLeftCorner, r_nextRoomDirection):
+	previousTopLeft = topLeftCorner
 	RandomizeWidthAndHeight()
-	setFloor(topLeftCorner)
-	setWalls(topLeftCorner)
-	setDoor(topLeftCorner,r_nextRoomDirection)
+	setFloor (topLeftCorner)
+	setWalls (topLeftCorner)
+	setDoor (topLeftCorner, r_nextRoomDirection)
 
-func setFloor(startPos):
-	for yPos in range(startPos.y,startPos.y+height):
-		for xPos in range(startPos.x,startPos.x + width):
-			tile_pos = Vector2(xPos,yPos)
-			set_cellv(tile_pos, _tileset.find_tile_by_name("Floor"))
+func setFloor (origin):
+	for yPos in range (origin.y,origin.y+height):
+		for xPos in range (origin.x,origin.x + width):
+			tile_pos = Vector2 (xPos,yPos)
+			set_cellv (tile_pos, _tileset.find_tile_by_name("Floor"))
 
 
-func setDoor(startPos,r_nextRoomDirection):
+func setDoor (startPos, r_nextRoomDirection):
 	random.randomize()
 	var r_value = startPos.x
 	print(r_nextRoomDirection)
-	if(r_nextRoomDirection == 0): #Down
+	if (r_nextRoomDirection == 0): #Down
 		random.randomize()
-		r_value = random.randi_range(startPos.y+1,startPos.y+height-2)
-		var doorPos = Vector2(startPos.x,r_value)
-		set_cellv(doorPos, _tileset.find_tile_by_name("Gate"))
+		var yOrigin = topLeftCorner.y - previousTopLeft.y
+		r_value = random.randi_range (startPos.y+1, startPos.y + height - yOrigin)
+		var doorPos = Vector2 (startPos.x, r_value)
+		set_cellv (doorPos, _tileset.find_tile_by_name("Gate"))
 		doorPos.y += 1
-		set_cellv(doorPos, _tileset.find_tile_by_name("Gate"))
+		set_cellv (doorPos, _tileset.find_tile_by_name("Gate"))
 	else: #Right
 		random.randomize()
-		r_value = random.randi_range(startPos.x+1,startPos.x+width-2)
+		var xOrigin = topLeftCorner.x - previousTopLeft.x
+		r_value = random.randi_range (startPos.x+1,startPos.x + width - xOrigin)
 		var doorPos = Vector2(r_value,startPos.y)
 		set_cellv(doorPos, _tileset.find_tile_by_name("Gate"))
 		doorPos.x += 1
 		set_cellv(doorPos, _tileset.find_tile_by_name("Gate"))	
 
 
-func setWalls(startPos):
-	for xPos in range(startPos.x,startPos.x + width):
-		tile_pos = Vector2(xPos,startPos.y)
-		set_cellv(tile_pos, _tileset.find_tile_by_name("Boundary"))
-	for xPos in range(startPos.x,startPos.x + width):
-		tile_pos = Vector2(xPos,startPos.y+height)
-		set_cellv(tile_pos, _tileset.find_tile_by_name("Boundary"))
-	for yPos in range(startPos.y,startPos.y + height):
-		tile_pos = Vector2(startPos.x,yPos)
-		set_cellv(tile_pos, _tileset.find_tile_by_name("Boundary"))
-	for yPos in range(startPos.y,startPos.y + height+1):
-		tile_pos = Vector2(startPos.x+width,yPos)
-		set_cellv(tile_pos, _tileset.find_tile_by_name("Boundary"))
+func setWalls (startPos):
+	for xPos in range (startPos.x,startPos.x + width):
+		tile_pos = Vector2 (xPos,startPos.y)
+		set_cellv (tile_pos, _tileset.find_tile_by_name("Boundary"))
+	for xPos in range (startPos.x,startPos.x + width):
+		tile_pos = Vector2 (xPos,startPos.y+height)
+		set_cellv (tile_pos, _tileset.find_tile_by_name("Boundary"))
+	for yPos in range (startPos.y,startPos.y + height):
+		tile_pos = Vector2 (startPos.x,yPos)
+		set_cellv (tile_pos, _tileset.find_tile_by_name("Boundary"))
+	for yPos in range (startPos.y,startPos.y + height+1):
+		tile_pos = Vector2 (startPos.x+width,yPos)
+		set_cellv (tile_pos, _tileset.find_tile_by_name("Boundary"))
 
 
 
 func RandomizeWidthAndHeight():
 	random.randomize()
+	prevWidth = width
+	prevHeight = height
 	width = random.randi_range(3,40)
 	height = random.randi_range(5,40)
